e3c478654dc7b62caf00b57c853b105c11d0ffda
unknown <rossmacbean@gmail.com>
2019-12-22 19:27:31 +0000

    refactor

--- src/render_gl.rs
+++ src/render_gl.rs
@@ -93,9 +93,7 @@ impl Shader {
 
 impl Drop for Shader {
     fn drop (&mut self) {
-        unsafe {
-            gl::DeleteShader(self.id);
-        }
+        unsafe { gl::DeleteShader(self.id); }
     }
 }
 
